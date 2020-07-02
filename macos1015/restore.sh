16bb768530f7f3f893e2014e92b10ffe807e3066
xiote <xiote@nate.com>
2020-07-02 14:06:05 +0900

    restore.sh 추가

---
+++ macos1015/restore.sh
@@ -0,0 +1,6 @@
+echo "Restore machine..." 
+vagrant snapshot restore macos1015
+echo "-------------------------" 
+echo "vagrant ssh for public ip" 
+echo "id/pwd: vagrant/vagrant" 
+echo "-------------------------" 
