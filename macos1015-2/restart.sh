2355d2b4e7677c1d4f0e86c3a5d6b8295402062f
xiote <xiote@nate.com>
2020-07-02 14:09:14 +0900

    macos1015-2 삭제

--- macos1015-2/restart.sh
+++
@@ -1,9 +0,0 @@
-echo "Destory machine..." 
-vagrant destroy -f
-echo "-------------------------" 
-echo "Start machine..." 
-vagrant up
-echo "-------------------------" 
-echo "vagrant ssh for public ip" 
-echo "id/pwd: vagrant/vagrant" 
-echo "-------------------------" 
